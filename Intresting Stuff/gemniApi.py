172078860ea7d3f566ecac1df9b56270847b2e7e
Edmundtutu <2021bse180@std.must.ac.ug>
2024-10-16 01:43:43 +0300

    more intresting things

---
+++ Intresting Stuff/gemniApi.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+import google.generativeai as genai
+
+load_dotenv()
+
+api_key = os.getenv("API_KEY")
+
+genai.configure(api_key=api_key)
+
+
+model = genai.GenerativeModel("gemini-1.5-flash")
+response = model.generate_content("Write a story about a magic backpack.")
+print(response.text)
+
