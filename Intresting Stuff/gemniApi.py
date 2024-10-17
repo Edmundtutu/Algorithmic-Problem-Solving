340ab55c641d3fd875b862ddc6ad3193f1f11e58
Edmundtutu <2021bse180@std.must.ac.ug>
2024-10-17 11:38:22 +0300

    Added some more intresting stuff

--- Intresting Stuff/gemniApi.py
+++ Intresting Stuff/gemniApi.py
@@ -8,7 +8,7 @@ api_key = os.getenv("API_KEY")
 
 genai.configure(api_key=api_key)
 
-
+print(api_key)
 model = genai.GenerativeModel("gemini-1.5-flash")
 response = model.generate_content("Write a story about a magic backpack.")
 print(response.text)
